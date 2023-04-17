d3a46ad42a2af8d4fa78153d09669d121fd9a2e3
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-18 00:33:54 +0300

    Записаны комментарии в admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -4,4 +4,10 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_display = ['title', 'slug', 'author', 'publish', 'status']  # отображение на странице списка полей
+    list_filter = ['status', 'created', 'publish', 'author']  # позволяет фильтровать результаты по полям
+    search_fields = ['title', 'body']  # строка поиска
+    prepopulated_fields = {'slug': ('title',)}  # предзаполнение поля slug, даннымии вводимыми в title
+    raw_id_fields = ['author']  # Поисковый виджет
+    date_hierarchy = 'publish'  # навигационные ссылки для навигации по иерархии дат
+    ordering = ['status', 'publish']  # атрибуты сортировки по умолчанию

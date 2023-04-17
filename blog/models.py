476aa2a4d535abf0dd40845ace43e4b2d1222cfa
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-17 23:27:22 +0300

    Добавление поля status

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,23 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        """
+        Добавление поля статуса, которое позволит управлять статусом постов блога. В постах будет использоваться статусы
+        Draft(черновик) и Published(опубликован)
+        """
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250, verbose_name='Заголовок')
     slug = models.SlugField(max_length=250, verbose_name='Слаг')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
         """

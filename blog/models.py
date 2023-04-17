491d0d0a20e066c61af3847baa94667f964e2af0
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-17 23:33:26 +0300

    Добавление поля author. Добавление взаимосвязи многие к одному

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -13,6 +15,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250, verbose_name='Заголовок')
     slug = models.SlugField(max_length=250, verbose_name='Слаг')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)

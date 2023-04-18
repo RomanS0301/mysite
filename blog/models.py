62b3c38de9355396e026c5d64f276a60953eff2e
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-18 21:51:24 +0300

    добавлен метод get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+# https://docs.djangoproject.com/en/4.1/ref/urlresolvers/#reverse
 from django.urls import reverse
 
 

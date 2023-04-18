76ab9b294d8d9d2fa8e6480ae6f9fefd64b1da9b
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-18 23:45:07 +0300

    Добавление обработчика исключений постраничной разбивки

--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
     post_list = Post.published.all()
     # Постраничная разбивка с 3 постами на страницу
+    # https://docs.djangoproject.com/en/4.1/ref/paginator/#paginator
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то выдать последнюю страницу
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   'blog/post/list.html',
                   {'posts': posts})

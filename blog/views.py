82b87275049fe558afbf1773ce78a1733f09ef33
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-20 00:39:14 +0300

    Отправление электронной почты с помощью представлений

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -55,10 +56,27 @@ def post_share(request, post_id):
     post = get_object_or_404(Post,
                              id=post_id,
                              status=Post.Status.PUBLISHED)  # Извлечь пост по идентификатору id
+    sent = False
     if request.method == 'POST':
         form = EmailPostForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data  # Поля формы успешно прошло валидацию. Отправить электронное письмо.
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url())
+            subject = f"{cd['name']} Рекомендовано к прочтению"\
+                      f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'roman.sidoruk@gmail.com',
+                      [cd['to']])
+            sent = True
+
+        else:
+            form = EmailPostForm()
+        return render(request, 'blog/post/share.html', {'post': post,
+                                                        'form': form,
+                                                        'sent': sent})
+
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,

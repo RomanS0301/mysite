b81e201640e9a6e645e7fbdb3a4303f35ce9b756
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-19 23:20:46 +0300

    добавлены комментарии

--- blog/forms.py
+++ blog/forms.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
+    name = forms.CharField(max_length=25)  # имя отправившего пост
+    email = forms.EmailField()  # электронная почта отправителя
+    to = forms.EmailField()  # электронная почта получателя
     comments = forms.CharField(required=False,
-                               widget=forms.Textarea)
+                               widget=forms.Textarea)  # поле комментариев

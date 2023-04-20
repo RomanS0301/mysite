2eed8f5f1813ab9f22babd0f979afc3039c4bcdf
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-20 23:14:47 +0300

    Создано представление post_comment, для отправки комментариев

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 def post_list(request):
@@ -81,3 +82,26 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    """
+    использовать это представление, чтобы управлять передачей поста на обработку
+    """
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Коментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        # Создать объект класса Comment не сохраняя его в базе данных
+        comment.post = post  # пост назначается созданному комментарию
+        # сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})

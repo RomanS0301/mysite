472da5ed1e5f640c10ee6bc139473f1224810fbb
Roman Sidoruk <roman.sidoruk@gmail.com>
2023-04-25 12:03:06 +0300

    Добавлен вывод 5 последних постов

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -9,6 +9,15 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     """
-    Создан шаблонный тег под конкретную задачу - вывести число опубликованных постов
+    Вывести число опубликованных постов
     """
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    """
+    Отображать последние посты на боковой панели блога
+    """
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
